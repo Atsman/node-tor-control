3848b1674ac6110608a88574f9d9585142792faf
Aleh Atsman <olegruno@gmail.com>
2017-03-20 17:27:55 +0300

    test(env): add chai as promised helper library

--- test/index.js
+++ test/index.js
@@ -2,8 +2,10 @@
 
 const chai = require('chai');
 const sinonChai = require('sinon-chai');
+const chaiAsPromised = require('chai-as-promised');
 
 chai.use(sinonChai);
+chai.use(chaiAsPromised);
 
 require('./connection.spec');
 require('./tor.spec');
